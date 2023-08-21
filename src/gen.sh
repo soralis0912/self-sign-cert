9618f08218593b1c34463c843436b1553de25598
soralis0912 <33514613+soralis0912@users.noreply.github.com>
2023-08-22 00:16:14 +0900

    有効期限を300日に短縮

--- src/gen.sh
+++ src/gen.sh
@@ -16,4 +16,4 @@ openssl req -new -key /out/server.key -subj "/C=AU/ST=Some-State/O=Self Sign Cer
 openssl ca -batch -extensions v3_ca -out /out/ca.crt -in /out/ca.csr -selfsign -keyfile /out/server.key
 
 # Generate server certificate with CA certificate.
-openssl x509 -days 3650 -req -extfile subjectnames.txt -CA /out/ca.crt -CAkey /out/server.key -set_serial 1 < /out/server.csr > /out/server.crt
+openssl x509 -days 300 -req -extfile subjectnames.txt -CA /out/ca.crt -CAkey /out/server.key -set_serial 1 < /out/server.csr > /out/server.crt
